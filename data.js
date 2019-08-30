a2ab35d04c5289a00ba2d5c92b0c34adef543918
nirustanite <nirustanite@gmail.com>
2019-08-30 13:27:43 +0200

    Added the first page and created some files

---
+++ data.js
@@ -0,0 +1,43 @@
+flashcards = {
+    dutch : [
+    {
+        word: "Goedemorgen",
+        translation: "Good-morning"
+    },
+    {
+        word: "Goedemiddag",
+        translation: "Good Afternoon"
+    },
+    {
+        word: "Welkom",
+        translation: "Welcome"
+    },
+    {
+        word: "Hoe ben jij?",
+        translation:"How are you?"
+    },
+    {
+        word: "Hoe heet jij?",
+        translation: "What is your name?"
+    },
+    {
+        word: "Welk land kom je?",
+        translation: "Which land are you coming from?"
+    },
+    {
+        word: "Wat is je geboortedatum?",
+        translation: "What is your date-of-birth?"
+    },
+    {
+        word: "Waar woon je?",
+        translation: "Where do you live"
+    },
+    {
+        word: "Aardappel",
+        translation: "potato"
+    },
+    {
+        word: "Bloemkool",
+        translation: "Cauliflower"
+    }]
+}
